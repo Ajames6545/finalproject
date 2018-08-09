3c2551c46ae13a1352d5ea56a1768ce33eafbed8
Code <Code@batc.local>
2018-08-09 11:16:00 -0600

    Final commit for CTS class.

--- src/components/planner.js
+++ src/components/planner.js
@@ -9,7 +9,7 @@ class Planner extends Component {
 
   clicked(data) {
     var element = document.getElementById(data);
-    element.setAttribute("style", "background-color: blue; color: white;");
+    element.setAttribute("style", "background-color:#E8E8E8;");
 
   }
 
@@ -22,12 +22,23 @@ class Planner extends Component {
   render() {
     return (
       <div>
+        <div class="bgimage"></div>
+        <div class="bgstrip"></div>
         <center>
           <table border="1">
+           <tr>
+             <td class="date">Sunday</td>
+             <td class="date">Monday</td>
+             <td class="date">Tuesday</td>
+             <td class="date">Wednesday</td>
+             <td class="date">Thursday</td>
+             <td class="date">Friday</td>
+             <td class="date">Saturday</td>
+           </tr> 
             <tr>
-              <td id="cell1"><input class="inputfields" onClick={() => this.clicked("cell1")}/></td>
-              <td id="cell2"><input class="inputfields" onClick={() => this.clicked("cell2")} /></td>
-              <td id="cell3"><input class="inputfields" onClick={() => this.clicked("cell3")} /></td>
+              <td id="cell1"></td>
+              <td id="cell2"></td>
+              <td id="cell3"></td>
               <td id="cell4">1<input class="inputfields" onClick={() => this.clicked("cell4")} /></td>
               <td id="cell5">2<input class="inputfields" onClick={() => this.clicked("cell5")} /></td>
               <td id="cell6">3<input class="inputfields" onClick={() => this.clicked("cell6")} /></td>
@@ -53,24 +64,25 @@ class Planner extends Component {
             </tr>
             <tr>
             <td id="cell22">19<input class="inputfields" onClick={() => this.clicked("cell22")} /></td>
-            <td id="cell23">21<input class="inputfields" onClick={() => this.clicked("cell23")} /></td>
-            <td id="cell24">22<input class="inputfields" onClick={() => this.clicked("cell24")} /></td>
-            <td id="cell25">23<input class="inputfields" onClick={() => this.clicked("cell25")} /></td>
-            <td id="cell26">24<input class="inputfields" onClick={() => this.clicked("cell26")} /></td>
-            <td id="cell27">25<input class="inputfields" onClick={() => this.clicked("cell27")} /></td>
-            <td id="cell28">26<input class="inputfields" onClick={() => this.clicked("cell28")} /></td>
+            <td id="cell23">20<input class="inputfields" onClick={() => this.clicked("cell23")} /></td>
+            <td id="cell24">21<input class="inputfields" onClick={() => this.clicked("cell24")} /></td>
+            <td id="cell25">22<input class="inputfields" onClick={() => this.clicked("cell25")} /></td>
+            <td id="cell26">23<input class="inputfields" onClick={() => this.clicked("cell26")} /></td>
+            <td id="cell27">24<input class="inputfields" onClick={() => this.clicked("cell27")} /></td>
+            <td id="cell28">25<input class="inputfields" onClick={() => this.clicked("cell28")} /></td>
             </tr>
             <tr>
-            <td id="cell29">27<input class="inputfields" onClick={() => this.clicked("cell29")} /></td>
-            <td id="cell30">28<input class="inputfields" onClick={() => this.clicked("cell30")} /></td>
-            <td id="cell31">29<input class="inputfields" onClick={() => this.clicked("cell31")} /></td>
-            <td id="cell32">30<input class="inputfields" onClick={() => this.clicked("cell32")} /></td>
-            <td id="cell33">31<input class="inputfields" onClick={() => this.clicked("cell33")} /></td>
-            <td id="cell34"><input class="inputfields" onClick={() => this.clicked("cell34")} /></td>
-            <td id="cell35"><input class="inputfields" onClick={() => this.clicked("cell35")} /></td>
+            <td id="cell29">26<input class="inputfields" onClick={() => this.clicked("cell29")} /></td>
+            <td id="cell30">27<input class="inputfields" onClick={() => this.clicked("cell30")} /></td>
+            <td id="cell31">28<input class="inputfields" onClick={() => this.clicked("cell31")} /></td>
+            <td id="cell32">29<input class="inputfields" onClick={() => this.clicked("cell32")} /></td>
+            <td id="cell33">30<input class="inputfields" onClick={() => this.clicked("cell33")} /></td>
+            <td id="cell34">31<input class="inputfields" onClick={() => this.clicked("cell34")} /></td>
+            <td id="cell35"></td>
             </tr>
           </table>
         </center>
+        <div class="bskew"></div>
       </div>
     );
   }
